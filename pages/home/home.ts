815e86cc6cb02171a62250f1b8f05bed5e3f3362
tobyapdujan <toby.apdujan110323@my.lsgh.edu.ph>
2021-01-20 09:34:36 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -13,9 +13,18 @@ export class HomePage {
   age: number;
   activitylevel: string;
   dcic: number;
+  bmr: number;
 
 calculateDCIC () {
+  this.gender = Male/Female
+
+  if (this.gender = Male) {
+    this.bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
+  }
+
+
+  }
   
 }
 
-}
+
